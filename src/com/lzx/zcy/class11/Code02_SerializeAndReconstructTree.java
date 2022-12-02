dd369fc3a6286bee006be409fd5a13bcb6ef132b
LZX-178 <1786081318@qq.com>
2022-12-02 17:00:45 +0800

    hp

--- src/com/lzx/zcy/class11/Code02_SerializeAndReconstructTree.java
+++ src/com/lzx/zcy/class11/Code02_SerializeAndReconstructTree.java
@@ -63,12 +63,12 @@ public class Code02_SerializeAndReconstructTree {
         }
     }
 
+    // 按先序序列化
     public Queue<String> preSerial(Node head) {
         Queue<String> ans = new LinkedList<>();
         preSerial(head, ans);
         return ans;
     }
-
     private void preSerial(Node head, Queue<String> ans) {
         if (head == null){
             ans.add(null);
@@ -79,34 +79,92 @@ public class Code02_SerializeAndReconstructTree {
         preSerial(head.right, ans);
     }
 
+    // 按先序反序列化, 为便于测试, 每个元素的值要诚实
     public Node buildByPreQueue(Queue<String> preList){
         Node head;
         String value = preList.poll();
         if (value == null) {
             return null;
         }else {
-            head = new Node(Integer.parseInt(value));
+            head = new Node(Integer.parseInt(value)*10);
             buildByPreQueue(preList, head);
         }
         return head;
     }
-
     private void buildByPreQueue(Queue<String> preList, Node node) {
         String value = preList.poll();
         if (value != null) {
-            node.left = new Node(Integer.parseInt(value));
+            node.left = new Node(Integer.parseInt(value)*10);
             buildByPreQueue(preList, node.left);
         }
 
         value = preList.poll();
         if (value != null) {
-            node.right = new Node(Integer.parseInt(value));
+            node.right = new Node(Integer.parseInt(value)*10);
             buildByPreQueue(preList, node.right);
         }
     }
 
+    // 按层序序列化
+    public Queue<String> levelSerial(Node head){
+        Queue<Node> queue = new LinkedList<>();
+        Queue<String> ans = new LinkedList<>();
+
+        queue.add(head);
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+
+            if (node == null) {
+                ans.add(null);
+            }else {
+                ans.add(String.valueOf(node.value));
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return ans;
+    }
+    // 按层序反序列化, 为便于测试, 每个元素的值要诚实
+    public Node buildByLevelQueue(Queue<String> preList){
+
+        String value = preList.poll();
+        if (value == null){
+            return null;
+        }
+        Node head = new Node(Integer.parseInt(value) * 10);
+
+        // queue 记录当前层的元素
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(head);
+
+        // 每次都遍历一层的元素
+        while (!preList.isEmpty()) {
+            // 遍历当前层的每一个元素, for循环其实可以省略
+//            for (int i = 0, width = queue.size(); i < width; i++) {
+                Node node = queue.poll();
+
+                // 如果左节点非空, 进行赋值,
+                // 并可能需要为 左节点 的左右节点赋值, 将其加入 queue,
+                value = preList.poll();
+                if (value != null) {
+                    node.left = new Node(Integer.parseInt(value) * 10);
+                    queue.add(node.left);
+                }
+
+                // 同上, 处理右节点
+                value = preList.poll();
+                if (value != null) {
+                    node.right = new Node(Integer.parseInt(value) * 10);
+                    queue.add(node.right);
+                }
+//            }
+        }
+        return head;
+    }
+
+
     @Test
-    public void test_Traversal1(){
+    public void test_preSerial1(){
         Node head;
         head = new Node(1);
         head.left = new Node(2);
@@ -122,8 +180,8 @@ public class Code02_SerializeAndReconstructTree {
         //   / \    / \
         //  4   5  6   7
 
-        PrintBinaryTree printer1 = new PrintBinaryTree(head);
-        printer1.print(false);
+        PrintBinaryTree printer = new PrintBinaryTree(head);
+        printer.print(false);
 
         Queue<String> serial = preSerial(head);
 
@@ -131,13 +189,12 @@ public class Code02_SerializeAndReconstructTree {
         System.out.println("serial = " + serial);
 
         System.out.println("***** " + "buildByPreQueue" + " *****");
-        Node newHead = buildByPreQueue(serial);
-        PrintBinaryTree printer2 = new PrintBinaryTree(newHead);
-        printer2.print(false);
+        printer.root = buildByPreQueue(serial);
+        printer.print(false);
     }
 
     @Test
-    public void test_Traversal2() {
+    public void test_preSerial2() {
         Node head = new Node(1);
         GenerateRandomBT randomBT = new GenerateRandomBT(head, 5, 20, 1, 100, 4);
         randomBT.generateRandomBT();
@@ -154,5 +211,55 @@ public class Code02_SerializeAndReconstructTree {
         Node newHead = buildByPreQueue(serial);
         PrintBinaryTree printer2 = new PrintBinaryTree(newHead);
         printer2.print(false);
+    }
+
+    @Test
+    public void test_leverSerial1() {
+        Node head;
+        head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        //       1
+        //      /  \
+        //    2      3
+        //   / \    / \
+        //  4   5  6   7
+
+        PrintBinaryTree printer = new PrintBinaryTree(head);
+        printer.print(false);
+
+        Queue<String> serial = levelSerial(head);
+        System.out.println("serial = " + serial);
+
+        Node newHead = buildByLevelQueue(serial);
+        printer.root = newHead;
+        printer.print(false);
+    }
+
+    @Test
+    public void test_leverSerial2() {
+
+        Node head = new Node(1);
+        GenerateRandomBT randomBT = new GenerateRandomBT(head, 5, 20, 1, 100, 4);
+        randomBT.generateRandomBT();
+
+        PrintBinaryTree printer = new PrintBinaryTree(head);
+        printer.print(false);
+
+        Queue<String> serial = levelSerial(head);
+
+        System.out.println("***** " + "serial" + " *****");
+        System.out.println("serial = " + serial);
+
+        System.out.println("***** " + "buildByLevelQueue" + " *****");
+        Node newHead = buildByLevelQueue(serial);
+        PrintBinaryTree printer2 = new PrintBinaryTree(newHead);
+        printer2.print(false);
+
     }
 }
