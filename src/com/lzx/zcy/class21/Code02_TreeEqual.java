7df4999325a4ec7a063550d6cadd6b019a285aea
LZX-178 <1786081318@qq.com>
2023-01-01 17:12:56 +0800

    hp

---
+++ src/com/lzx/zcy/class21/Code02_TreeEqual.java
@@ -0,0 +1,10 @@
+package com.lzx.zcy.class21;
+
+/**
+ * @author LZX
+ * @code @create 2023-01-01 17:10:27
+ * 给定两颗二叉树 big 和 small, 判断 small 是否为 big 的一颗子树
+ *      将 big 和 small 序列化后, 比较对两个序列进行 kmp 比较
+ */
+public class Code02_TreeEqual {
+}
